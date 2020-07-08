4b9c476bbfc8a7c9fd2edb2cad789f89da03834f
Pooja Sinha <42613961+psin007@users.noreply.github.com>
2020-07-08 13:10:50 +1000

    Add files via upload
    completed- all unique characters in a string using hashmap

--- AllUniqueCharInString.java
+++ AllUniqueCharInString.java
@@ -5,6 +5,8 @@
  */
 package crackingthecodinginterview;
 
+import java.util.HashMap;
+
 /**
  *program checks if all characters are unique in given string
  * @author Pooja
@@ -13,14 +15,24 @@ public class AllUniqueCharInString {
     
     
     public boolean checkUniqueness (String s){
+       HashMap<Character,Integer> hash = new HashMap<>();
+       for( char c: s.toCharArray()){
+           if(hash.containsKey(c)){
+            return false;
+           }
+           else{
+               hash.put(c,1);
+           }
+       }
         return true;
     }
     
     public static void main(String args[]){
         AllUniqueCharInString obj = new AllUniqueCharInString();
-        boolean returnValue = obj.checkUniqueness("Pooja");
-        String result = (returnValue = true) ? "All unique":"repeating characters";
+        boolean returnValue = obj.checkUniqueness("abcdefg");
+        String result = (returnValue == true) ? "All unique":"repeating characters";
         System.out.println(result);
+        
                 
     } 
 }
