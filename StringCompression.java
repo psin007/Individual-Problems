5dff6b8ba5b243db1571b99a7c9c61f491d6f83d
Pooja Sinha <42613961+psin007@users.noreply.github.com>
2020-07-09 16:01:22 +1000

    String compression

---
+++ StringCompression.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package crackingthecodinginterview;
+
+/**
+ *
+ * @author Pooja
+ */
+public class StringCompression {
+    
+    String compressedString(String s){
+       
+        int count = 1,index = 0, i = 0;
+        boolean flag = true;
+        StringBuilder sb = new StringBuilder();
+        while(i<s.length()){
+            sb.append(s.charAt(i));
+            while(flag && i+1 < s.length()){
+                if(s.charAt(i) == s.charAt(i+1)){
+                    count++;
+                    i++;
+                }
+                else{
+                    flag = false;
+                }
+            }
+            if(count > 1){
+                sb.append(count);
+                count = 1;
+            }
+            flag = true;
+            i++;
+        }
+        String newS = sb.toString();
+        return s.length() > newS.length() ? newS :s;
+        
+    }
+    
+    public static void main(String args[]){
+        StringCompression obj = new StringCompression();
+        String[] s = {"aabcccccaa","abc"};
+        for(String str: s){
+            System.out.println(obj.compressedString(str));
+        }
+    }
+}
